bb3097cadb2fdcee3c4e1d1e4d9a57f7906ea230
Etienne G <crazypops@gmail.com>
2018-11-15 00:36:33 +0100

    Removed constants

--- main.go
+++ main.go
@@ -13,14 +13,8 @@ import (
 )
 
 const (
-	baseURL                    = "https://api.twitch.tv/helix"
-	getGamesEndpoint           = "/games"
-	getStreamsEndpoint         = "/streams"
-	getStreamsMetadataEndpoint = "/streams/metadata"
-	getUsersEndpoint           = "/users"
-	getFollowsEndpoint         = "/follows"
-	updateUserEndpoint         = "/users"
-	getVideosEndpoint          = "/videos"
+	baseURL            = "https://api.twitch.tv/helix"
+	getStreamsEndpoint = "/streams"
 )
 
 func main() {
